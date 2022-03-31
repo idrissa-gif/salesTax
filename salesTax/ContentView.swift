a8184bf8bb6f52047a8dbb86f4f18f66230fa307
idrissa-gif <idrissa@iut-dhaka.edu>
2022-03-31 23:15:30 +0600

    Initial Commit

---
+++ salesTax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  salesTax
+//
+//  Created by idrissa Mahamoudou Dicko on 31/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ salesTax/salesTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  salesTaxApp.swift
+//  salesTax
+//
+//  Created by idrissa Mahamoudou Dicko on 31/3/22.
+//
+
+import SwiftUI
+
+@main
+struct salesTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
